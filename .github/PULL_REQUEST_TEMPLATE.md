451f070d09140ef9044838a124fa192ded649df1
Alexandru RADOVICI <alexandru.radovici@wyliodrin.com>
2025-11-20 21:39:32 +0200

    add settings for editors and github workflow

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,24 @@
+### Pull Request Overview
+
+This pull request adds/changes/fixes...
+
+
+### Testing Strategy
+
+This pull request was tested by...
+
+
+### TODO or Help Wanted
+
+This pull request still needs...
+
+
+### Build
+
+- [ ] Ran `cargo fmt`.
+- [ ] Ran `cargo build`.
+- [ ] Ran `cargo clippy`.
+
+### Author
+
+Signed-off-by: Your Name <your.email@example.com>
--- host/usb-bulk-python/main.py
+++ host/usb-bulk-python/main.py
@@ -1,16 +1,17 @@
+import time
+
 import usb.core
 import usb.util
-import time
 
 while True:
     # find our device
     #   VendorID:  0xc0de
     #   ProductID: 0xcafe
-    dev = usb.core.find(idVendor=0xc0de, idProduct=0xcafe)
+    dev = usb.core.find(idVendor=0xC0DE, idProduct=0xCAFE)
 
     # was it found?
     if dev is None:
-        print ("Device not found, retying in 1s")
+        print("Device not found, retying in 1s")
         time.sleep(1)
 
     else:
@@ -22,23 +23,21 @@ while True:
 
             # get an endpoint instance
             cfg = dev.get_active_configuration()
-            intf = cfg[(0,0)]
+            intf = cfg[(0, 0)]
 
             ep_out = usb.util.find_descriptor(
                 intf,
                 # match the first OUT endpoint
-                custom_match = \
-                lambda e: \
-                    usb.util.endpoint_direction(e.bEndpointAddress) == \
-                    usb.util.ENDPOINT_OUT)
+                custom_match=lambda e: usb.util.endpoint_direction(e.bEndpointAddress)
+                == usb.util.ENDPOINT_OUT,
+            )
 
             ep_in = usb.util.find_descriptor(
                 intf,
                 # match the first IN endpoint
-                custom_match = \
-                lambda e: \
-                    usb.util.endpoint_direction(e.bEndpointAddress) == \
-                    usb.util.ENDPOINT_IN)
+                custom_match=lambda e: usb.util.endpoint_direction(e.bEndpointAddress)
+                == usb.util.ENDPOINT_IN,
+            )
 
             assert ep_out is not None
             assert ep_in is not None
@@ -49,10 +48,10 @@ while True:
 
             # write the data
             for i in range(0, 1000000):
-                ep_out.write(f'test {i}')
+                ep_out.write(f"test {i}")
                 value = ep_in.read(100)
-                print(str(value, 'ascii'))
+                print(str(value, "ascii"))
                 time.sleep(1)
 
         except Exception as error:
-            print (f'Error {error}')
+            print(f"Error {error}")
